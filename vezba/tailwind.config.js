3f4d4bfd6a8c8507f795fdafb29a96b241fa5e4d
andrejbasovski <136736617+andrejbas@users.noreply.github.com>
2024-11-10 23:48:15 +0100

    Add files via upload

---
+++ vezba/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+	theme: {
+		extend: {
+      
+			backgroundSize: {
+				'400': '400%',
+			  },
+		},
+	},
+	plugins: [],
+};
